9d96f87bc29fa525107896ace1d08dc7fb3121f4
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2019-11-19 10:39:53 -0600

    created a backend

---
+++ resources/views/layouts/_includes/backendNav.blade.php
@@ -0,0 +1,69 @@
+<div class="main-navigation fixed-top site-header border-bottom px-0" id="mainmenu-area">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container align-items-center">
+                <a class="navbar-brand m-1" href="index.html">
+                    <h2 class="mb-0"><span class="text-color">Molly Felder</h2>
+                </a>
+                <!-- Toggler -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarmain" aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="ti-menu-alt"></span>
+                </button>
+
+                <!-- Collapse -->
+                <div class="collapse navbar-collapse text-center text-lg-left" id="navbarmain">
+                    <!-- Links -->
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item ">
+                            <a href="#banner" class="nav-link smoth-scroll">
+                                Home
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a href="#about" class="nav-link smoth-scroll">
+                                About
+                            </a>
+                        </li>
+
+                         <li class="nav-item ">
+                            <a href="#reviews" class="nav-link smoth-scroll">
+                                Reviews
+                            </a>
+                        </li>
+
+                        <li class="nav-item ">
+                            <a href="#author" class="nav-link smoth-scroll">
+                                About Me
+                            </a>
+                        </li>
+
+                        <li class="nav-item ">
+                            <a href="#additionalBooks" class="nav-link smoth-scroll">
+                                More Books
+                            </a>
+                        </li>
+
+                        <li class="nav-item ">
+                            <a href="#events" class="nav-link smoth-scroll">
+                                Events
+                            </a>
+                        </li>
+
+                        <li class="nav-item ">
+                            <a href="#contact" class="nav-link smoth-scroll">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+
+                    <ul class="list-inline top-socials mb-0">
+                        <li class="list-inline-item">
+                            <a href="#"><i class="ti-facebook"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#"><i class="ti-twitter"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
---
+++ resources/views/layouts/backend.blade.php
@@ -0,0 +1,9 @@
+@include('layouts/_includes/header')
+
+<body id="top-header">
+    @include('layouts/_includes/backendNav')
+    <main>
+        @yield('content')
+    </main>
+    @include('layouts/_includes/footer')
+</body>
